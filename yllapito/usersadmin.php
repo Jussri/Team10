70c8986e60bea3dccdfcc6753b36c07104358f17
Jussri <jussi21007@student.hamk.fi>
2022-02-16 20:16:19 +0200

    add
    htaccess

--- yllapito/usersadmin.php
+++ yllapito/usersadmin.php
@@ -13,7 +13,8 @@ $tulos=mysqli_query($yhteys, "select * from users");
 
 print "<ol>";
 while ($rivi=mysqli_fetch_object($tulos)){
-    print "<li>$rivi->firstname $rivi->lastname</li>";
+    print "<li>$rivi->firstname $rivi->lastname"."<a href='./poista.php?poistettava=$rivi->id'> Poista</a> ". 
+    "<a href='./muokkaa.php?muokattava=$rivi->id'> Muokkaa</a></li>";
 }
 print "</ol>";
 
