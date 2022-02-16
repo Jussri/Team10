653505308defa5c2ec24edcdb51ba2d69979fed5
Joonark <91543808+Joonark@users.noreply.github.com>
2022-02-16 12:32:25 +0200

    link changes

--- php/register.php
+++ php/register.php
@@ -1,6 +1,6 @@
 <?php
 
-if isset($_POST["email"]) && isset($_POST["password"]) &&
+if (isset($_POST["email"]) && isset($_POST["password"]) &&
 isset($_POST["firstname"]) && isset($_POST["lastname"])) {
 $email=$_POST["email"];
 $password=$_POST["password"];
