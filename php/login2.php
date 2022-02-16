c9c24c986aa77e16406d26cd95e8b898a4531601
Joonark <91543808+Joonark@users.noreply.github.com>
2022-02-16 12:17:07 +0200

    joona

--- php/login2.php
+++ php/login2.php
@@ -5,7 +5,7 @@
 <?php
 print "<h2> Log in<h2>";
 ?>
-<form action='login.php' method='post'>
+<form action='checklogin.php' method='post'>
     E-mail: <input type='text' name='email' value=''> <br>
     Password: <input type='password' name='password' value=''><br>
     <input type='submit' name='ok' value='OK'>
