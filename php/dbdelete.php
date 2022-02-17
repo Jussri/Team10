00f29a1f132f12002cbee28b2ec67e4ef0cd6839
Joonark <91543808+Joonark@users.noreply.github.com>
2022-02-17 14:05:56 +0200

    joona

--- php/dbdelete.php
+++ php/dbdelete.php
@@ -26,8 +26,8 @@ $tulos=mysqli_query($yhteys, "select * from users");
 
 print "<ol>";
 while ($rivi=mysqli_fetch_object($tulos)){
-    print "<liv>id=$rivi->id firstname=$rivi->firstname lastname=$rivi->lastname email=$rivi->email
-     password=$rivi->password usertype=$rivi->usertype <a href='dbdelete.php?poistettava=".$rivi->id."'>Delete</a><a href='modify.php?modify=".rivi->id."'<br>";
+    print "<liv>id=$rivi->id firstname=$rivi->firstname lastname=$rivi->lastname email=$rivi->email password=$rivi->password usertype=$rivi->usertype " . 
+    "<a href='dbdelete.php?poistettava=".$rivi->id."'>Delete</a> <a href='modify.php?modify=".$rivi->id."'>Modify</a><br>";
 }
 print "</ol>";
 mysqli_close($yhteys);
