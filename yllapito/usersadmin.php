f1508dd189688942e646f866338e0ac9ed88cecd
Jussri <jussi21007@student.hamk.fi>
2022-02-17 15:10:37 +0200

    change
    sad

--- yllapito/usersadmin.php
+++ yllapito/usersadmin.php
@@ -1,23 +1,23 @@
 <?php
 
 try{
-    $yhteys=mysqli_connect("db", "root", "password", "users");
+    $connection=mysqli_connect("db", "root", "password", "users");
 }
 catch(Exception $e){
     header("Error!");
     exit;
 }
 
-$tulos=mysqli_query($yhteys, "select * from users");
+$results=mysqli_query($connection, "select * from users");
 
 
 print "<ol>";
-while ($rivi=mysqli_fetch_object($tulos)){
-    print "<li>$rivi->firstname $rivi->lastname"."<a href='./php/dbdelete.php?poistettava=$rivi->id'>Delete</a> ". 
-    "<a href='../php/modify.php?modify=$rivi->id'> Edit data</a></li>";
+while ($row=mysqli_fetch_object($results)){
+    print "<li>$row->firstname $row->lastname"." <a href='./php/dbdelete.php?poistettava=$row->id'>Delete</a> ". 
+    "<a href='../php/modify.php?modify=$row->id'> Edit data</a></li>";
 }
 print "</ol>";
 
-mysqli_close($yhteys);
+mysqli_close($connection);
 ?>
 
