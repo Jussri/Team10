95420c5ada535c92d60e7f03fe80ed167d45271b
Jussri <jussi21007@student.hamk.fi>
2022-02-20 22:17:15 +0200

    Update login4.php

--- php/login4.php
+++ php/login4.php
@@ -11,8 +11,8 @@ if(isset($_POST['submit']))
 {
     $unm=mysqli_real_escape_string($con,$_POST['email']);
     $ps=mysqli_real_escape_string($con,$_POST['password']);
-
-    $sql="select password,usertype from users where email='$unm'";
+    $pshsa2= hash("sha256", $ps);
+    $sql="select password,usertype from users where email='$unm' and password='$pshsa2'";
     $data=mysqli_query($con,$sql);
     $count=mysqli_num_rows($data);
 
@@ -22,20 +22,29 @@ if(isset($_POST['submit']))
         $_SESSION['ROLE']=$row['usertype'];
         $_SESSION['IS_LOGIN']='yes';
 
-        if($row['password']==$ps)
+        if($row ['usertype']=='admin')
+        
         {
-           if($row ['usertype']=='admin')
-           {
-                header("Location:../yllapito/usersadmin.php");
-           }
-           if($row ['usertype']=='user')
-           {
-                 header("Location:../php/users.php");
-           }
+            header("Location:../yllapito/usersadmin.php");
+            exit;
         }
+<<<<<<< HEAD
+        if($row ['usertype']=='user')
+        {
+            
+            header("Location:../php/users.php");
+            exit;
+=======
         else{
-            header("Location:login.html");
+            header("Location:../login.html");
+>>>>>>> 5c97cc2fea5847f9c0eae913ea1b3d260c72954a
         }
+                      
+    } else {
+        
+       header("Location:../login.html");   
+       exit;
     }
+ 
 }
 ?>
